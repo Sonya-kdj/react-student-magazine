5dec625bc468f431c7ed4fb850b6bd6eaa5960fd
Soooyaaa <81586886+Sonya-kdj@users.noreply.github.com>
2024-05-03 23:16:56 +0300

    StudentBlock.jsx

--- src/components/StudentBlock.jsx
+++ src/components/StudentBlock.jsx
@@ -1,63 +1,78 @@
-import React, { useState } from 'react'
-import './StudentBlock.css'
+import React, { useState } from 'react';
+import './StudentBlock.css';
 
 const StudentBlock = ({ studentsArray }) => {
-	const [selectedStudents, setSelectedStudents] = useState([])
-	const [selectedDate, setSelectedDate] = useState('')
+  const [selectedStudents, setSelectedStudents] = useState([]);
+  const [selectedDate, setSelectedDate] = useState('');
 
-	const toggleSelection = studentId => {
-		setSelectedStudents(prevSelectedStudents => {
-			if (prevSelectedStudents.includes(studentId)) {
-				return prevSelectedStudents.filter(id => id !== studentId)
-			} else {
-				return [...prevSelectedStudents, studentId]
-			}
-		})
-	}
+  const toggleSelection = (studentId) => {
+    setSelectedStudents((prevSelectedStudents) => {
+      if (prevSelectedStudents.includes(studentId)) {
+        return prevSelectedStudents.filter((id) => id !== studentId);
+      }
+      return [...prevSelectedStudents, studentId];
+    });
+  };
 
-	const dateChange = event => {
-		setSelectedDate(event.target.value)
-	}
+  const dateChange = (event) => {
+    setSelectedDate(event.target.value);
+  };
 
-	const save = () => {
-		const studentsData = studentsArray.map(student => ({
-			id: student.id,
-			event: selectedStudents.includes(student.id),
-		}))
+  const save = async () => {
+    const studentsData = studentsArray.map((student) => ({
+      id: student.id,
+      event: selectedStudents.includes(student.id),
+    }));
 
-		console.log('Данные отправленные на сервер:', {
-			students: studentsData,
-			date: selectedDate,
-		})
-	}
+    const dataSend = {
+      students: studentsData,
+      date: selectedDate,
+    };
 
-	const studentElements = studentsArray.map(student => (
-		<div className='checkbox_container' key={student.id}>
-			<div className='student_wrap'>
-				<p className='studentsList'>
-					{student.first} {student.last}
-				</p>
-				<input
-					type='checkbox'
-					className='checkbox'
-					checked={selectedStudents.includes(student.id)}
-					onChange={() => toggleSelection(student.id)}
-				/>
-			</div>
-		</div>
-	))
+    try {
+      const response = await fetch('https://my-json-server.typicode.com/kosipov/1425-iro-placeholder-api/disciplines', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(dataSend),
+      });
 
-	return (
-		<div className='StudentContainer'>
-			<div className='dateTime'>
-				<input type='date' value={selectedDate} onChange={dateChange} />
-			</div>
-			{studentElements}
-			<button className='btn' onClick={save}>
-				Сохранить
-			</button>
-		</div>
-	)
-}
+      if (response.ok) {
+        console.log('Данные отправлены на сервер');
+      }
+    } catch (error) {
+      console.log('Ошибка')
+    }
+  };
 
-export default StudentBlock
+  const studentElements = studentsArray.map((student) => (
+    <div className="checkbox_container" key={student.id}>
+      <div className="student_wrap">
+        <p className="studentsList">
+          {student.first} {student.last}
+        </p>
+        <input
+          type="checkbox"
+          className="checkbox"
+          checked={selectedStudents.includes(student.id)}
+          onChange={() => toggleSelection(student.id)}
+        />
+      </div>
+    </div>
+  ));
+
+  return (
+    <div className="StudentContainer">
+      <div className="dateTime">
+        <input type="date" value={selectedDate} onChange={dateChange} />
+      </div>
+      {studentElements}
+      <button className="btn" onClick={save}>
+        Сохранить
+      </button>
+    </div>
+  );
+};
+
+export default StudentBlock;
